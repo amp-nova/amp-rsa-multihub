8c4eb7eb039c94c179b7b10019d4c89234b826cf
Reza Kalfane <reza.kalfane@gmail.com>
2021-03-10 10:52:19 +0100

    Refactoring 1

---
+++ resolvers.js
@@ -0,0 +1,41 @@
+const { products, categories } = require ('./mock-data');
+
+const resolvers = {
+  Query: {
+    products: () => products,
+    productById: (parent, args, context, info) => {
+      return products.find(x => x.id === args.id);
+    },
+    productBySku: (parent, args, context, info) => {
+      return products.find(x => x.sku === args.sku);
+    },
+    productBySlug: (parent, args, context, info) => {
+      return products.find(x => x.slug === args.slug);
+    },
+    categories: () => categories,
+    categoryById: (parent, args, context, info) => {
+      return categories.find(x => x.id === args.id);
+    },
+    categoryBySlug: (parent, args, context, info) => {
+      return categories.find(x => x.slug === args.slug);
+    }
+  },
+  Category: {
+    children(parent, args, context, info) {
+      const childrenList = parent.children;
+      return childrenList && categories.filter(x => childrenList.includes(x.id));
+    },
+    products(parent, args, context, info) {
+      const productsList = parent.products;
+      return productsList && products.filter(x => productsList.includes(x.id));
+    }
+  },
+  Product: {
+    categories(parent, args, context, info) {
+      const categoriesList = parent.categories;
+      return categoriesList && categories.filter(x => categoriesList.includes(x.id));
+    }
+  }
+};
+
+exports.resolvers = resolvers;
---
+++ typeDefs.js
@@ -0,0 +1,55 @@
+const { gql } = require('apollo-server');
+
+const typeDefs = gql`
+  type Product {
+    id: String!
+    sku: String!
+    slug: String!
+    name: String!
+    categories: [Category]
+    shortDescription: String
+    longDescription: String
+    prices: Prices
+    defaultImage: Image
+    images: [Image!]
+    variants: [Variant!]
+  }
+
+  type Prices {
+    sale: Float
+    list: Float
+  }
+
+  type Variant {
+    id: String!
+    prices: Prices
+    defaultImage: Image
+    images: [Image]
+  }
+
+  type Image {
+    title: String
+    alt: String
+    url: String!
+  }
+
+  type Category {
+    id: String!
+    name: String!
+    slug: String!
+    children: [Category]
+    products: [Product]
+  }
+
+  type Query {
+    products: [Product]
+    productById(id: String!): Product
+    productBySku(sku: String!): Product
+    productBySlug(slug: String!): Product
+    categories: [Category]
+    categoryById(id: String!): Category
+    categoryBySlug(slug: String!): Category
+  }
+`;
+
+exports.typeDefs = typeDefs;
