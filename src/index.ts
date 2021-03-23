c8367479ec46d56a5e6974884543902d4dfb03a7
dave lilly <dave.lilly@gmail.com>
2021-03-23 12:38:46 -0500

    added introspection to graphql server

--- src/index.ts
+++ src/index.ts
@@ -2,7 +2,7 @@ const { ApolloServer } = require('apollo-server');
 const { typeDefs } = require('./schemas/typeDefs');
 const { resolvers } = require('./resolvers/resolvers');
 
-const server = new ApolloServer({ typeDefs, resolvers, playground: true });
+const server = new ApolloServer({ typeDefs, resolvers, playground: true, introspection: true });
 server.listen(process.env.PORT || 4000).then(({ url }) => {
   console.log(`🚀  Server ready at ${url}`);
 });
