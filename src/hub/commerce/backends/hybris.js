9f1740aae1329aaf8fffec7a2c11073898a58c7e
dave lilly <dave@lillys.org>
2021-06-24 10:48:58 -0500

    add slug to hybris category

--- src/hub/commerce/backends/hybris.js
+++ src/hub/commerce/backends/hybris.js
@@ -7,6 +7,7 @@ const CommerceBackend = require('./index')
 const mapCategory = args => category => ({
     id: category.id,
     name: category.name || category.id,
+    slug: category.id,
     children: _.map(category.subcategories, mapCategory(args))
 })
 
