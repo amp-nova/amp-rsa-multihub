c5636f678723375bc7e66f34f8b40725a6a8d9d5
Dave Lilly <dave@lillys.org>
2021-08-25 19:58:34 -0500

    test

--- dist/util/config.js
+++ dist/util/config.js
@@ -1,6 +1,19 @@
 const { readFileSync } = require('fs');
 const yaml = require('js-yaml');
+const fs = require('fs-extra');
+const { execAsync } = require('./helpers');
 // Reading global settings
 const settingsYAML = readFileSync(`${__dirname}/../../config/settings.yaml`).toString();
 // Converting from YAML to JSON
-module.exports = yaml.load(settingsYAML);
+let settings = yaml.load(settingsYAML);
+let packageJson = fs.readJSONSync('./package.json');
+let cli = {};
+module.exports = {
+    ...settings,
+    packageJson,
+    cli,
+    init: async () => {
+        let result = await execAsync('git rev-parse --abbrev-ref HEAD');
+        cli['git'] = result.stdout.trim();
+    }
+};
