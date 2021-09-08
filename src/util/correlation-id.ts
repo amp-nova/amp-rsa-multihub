3b130bef150a67494174a7fc5754837fb997fb75
Dave Lilly <dave@lillys.org>
2021-09-08 10:42:43 -0500

    fix

--- src/util/correlation-id.ts
+++ src/util/correlation-id.ts
@@ -23,24 +23,26 @@ const getAmplienceConfigFromHeaders = headers => {
 
 router.use(async (req, res, next) => {
     if (req.path.indexOf('graphql') > -1 || req.path.indexOf('api/') > -1) {
-        let { appContext } = getAmplienceConfigFromHeaders(req.headers)
+        if (req.body?.operationName !== 'IntrospectionQuery') {
+            let { appContext } = getAmplienceConfigFromHeaders(req.headers)
 
-        const appUrl = url.parseURL(appContext.url)
-        const bareHost = _.first(appUrl.host.split('.'))
-        const graphqlOrigin = url.serializeURLOrigin(url.parseURL(config.app.host))
-        const backendKey = req.headers['x-arm-backend-key'] || req.headers['x-commerce-backend-key']
+            const appUrl = url.parseURL(appContext.url)
+            const bareHost = _.first(appUrl.host.split('.'))
+            const graphqlOrigin = url.serializeURLOrigin(url.parseURL(config.app.host))
+            const backendKey = req.headers['x-arm-backend-key'] || req.headers['x-commerce-backend-key'] || ''
+            
+            const tag = req.path.indexOf('graphql') > -1 ? req.body.operationName || `anonymousQuery` : req.path.split('/').pop()
+            req.correlationId = `${bareHost}-${backendKey.replace('/', '-')}-${tag}-${nanoid(4)}`
+            req.headers['x-arm-correlation-id'] = req.correlationId
         
-        const tag = req.path.indexOf('graphql') > -1 ? req.body.operationName || `anonymousQuery` : req.path.split('/').pop()
-        req.correlationId = `${bareHost}-${backendKey.replace('/', '-')}-${tag}-${nanoid(4)}`
-        req.headers['x-arm-correlation-id'] = req.correlationId
-    
-        logger.info(`${graphqlOrigin}/logs/${req.correlationId}`)
-    
-        req.hub = await hub({
-            backendKey,
-            logger: tag !== 'IntrospectionQuery' && await logger.getObjectLogger(req.correlationId),
-            ...getAmplienceConfigFromHeaders(req.headers)
-        })
+            logger.info(`${graphqlOrigin}/logs/${req.correlationId}`)
+        
+            req.hub = await hub({
+                backendKey,
+                logger: tag !== 'IntrospectionQuery' && await logger.getObjectLogger(req.correlationId),
+                ...getAmplienceConfigFromHeaders(req.headers)
+            })
+        }
     }
 
     next()
