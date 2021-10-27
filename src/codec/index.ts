d384e86036bed38e8b41f40b9cfdb58a2fc44935
Dave Lilly <dave@lillys.org>
2021-10-26 22:52:00 -0500

    .

--- src/codec/index.ts
+++
@@ -1,7 +0,0 @@
-export * from '../types'
-export * from './codec'
-export * from './codec-manager'
-export * from '../operation'
-
-import * as Types from '@amp-nova/aria-types'
-export { Types }
---
+++ src/index.ts
@@ -0,0 +1,7 @@
+export * from './types'
+export * from './codec/codec'
+export * from './codec/codec-manager'
+export * from './operation'
+
+import * as Types from '@amp-nova/aria-types'
+export { Types }
