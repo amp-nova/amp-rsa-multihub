3886f62d4ad08077a2057defdde6e49882511edf
Dave Lilly <dave@lillys.org>
2022-02-02 21:22:48 -0600

    remove translation client, aws libraries, fix audit issues

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,4 @@
 export * from './types'
 export * from './codec/codec'
 export * from './codec/codec-manager'
-import { awsSecretManagerLookupStrategy } from './codec/codec-manager'
 export * from './operation'
-
-const CredentialLookupStrategies = {
-    awsSecretManager: awsSecretManagerLookupStrategy
-}
-export { CredentialLookupStrategies }
